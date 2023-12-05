ba88fd28e0a52d4b459a64eb7714424d1bdbba8b
승희 <bangseunghee1108@gmail.com>
2023-12-05 11:30:31 +0900

    컴파일러 전

---
+++ typescript-comfile.ts
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @param a number
+ * @param b number
+ * @returns number
+ * 
+ * @description
+ * typescript는 '원하는 타입'을 사용하고 싶다는 javascript 유저의 강력한 니즈에서 시작되었다고 해도 과언이 아니다.
+ * 아래의 함수 add를 확인하였을 때 a는  number 타입이라는 약속, b도 number 타입만 받겠다는 제작자의 의지(?)를 확인할 수 있고 심지어 리턴값까지도 :number 라는 표기를 통해
+ * 해당 함수가 실행되어 완료되면 반환값의 데이터타입까지도 함수명 옆에 표기하며
+ * 어마어마한 가독성을 자랑한다.
+ * 코드는 길어졌지만, 가독성은 높아지는 현상이 t일어나고 있다.
+ * 매우 큰 코드베이스를 다루는 경우, 이러한 가독성은 매우 중요하다.
+ * 
+ */
+
+function add(a:number, b:number):number {
+  return a + b ;
+}
+
+console.log(add(1,2));
