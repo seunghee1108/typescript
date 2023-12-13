2040963087e7271a8b1f8d866c7a8798baaf8508
승희 <bangseunghee1108@gmail.com>
2023-12-14 01:47:55 +0900

    classroom-typescript-3-js-5

---
+++ typescript-3-5.js
@@ -0,0 +1,5 @@
+import { createComponent } from "./typescript-3-4";
+
+const myComponent = createComponent('div', { id: 'example', style: { color: 'red' } }, ['이것자스렸다']);
+const root = document.getElementById('root');
+root.innerHTML = myComponent;
