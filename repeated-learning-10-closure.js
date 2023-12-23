fdeb1529b8f93bd758f8c2f3894fe46028e8bb12
승희 <bangseunghee1108@gmail.com>
2023-12-23 23:30:42 +0900

    classroom-Repeated learning-10 Closure

---
+++ repeated-learning-10-closure.js
@@ -0,0 +1,22 @@
+function Pokemon(name, type) {
+  let privateName = name;
+  let privateType = type;
+
+  return {
+    getName: function() {
+      return privateName;
+    },
+    getType: function() {
+      return privateType;
+    },
+    setType: function(newType) {
+      privateType = newType;
+    }
+  };
+}
+
+const pikachu = Pokemon("피카츄", "전기");
+console.log(pikachu.getName());  // 피카츄
+console.log(pikachu.getType());  // 전기
+pikachu.setType("노말");
+console.log(pikachu.getType);  
